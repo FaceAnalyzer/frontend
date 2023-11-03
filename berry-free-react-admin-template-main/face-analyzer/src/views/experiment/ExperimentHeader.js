ea0fd0f059edc59e5a556b86428470c1dfb26826
Jo Falck-Ytter <jofa@stud.ntnu.no>
2023-11-03 15:21:49 +0100

    before rewriting experimentheader

--- berry-free-react-admin-template-main/face-analyzer/src/views/experiment/ExperimentHeader.js
+++ berry-free-react-admin-template-main/face-analyzer/src/views/experiment/ExperimentHeader.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Button, Typography, Box } from '@mui/material';
 import { IconEdit, IconTrashOff } from '@tabler/icons';
 
-
 const ExperimentHeader = () => {
+
     return (
         <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%' }}>
             <Typography variant="h4" sx={{ mb: 2 }}>
@@ -11,6 +11,14 @@ const ExperimentHeader = () => {
                 Experiment Title 
             </Typography>
             <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <Box sx={{ ml: 1 }}>
+                    <Button variant="contained" color="info" >
+                        {/* href/onclick should open a dialog box asking for conformation for edit*/}
+                        <IconEdit color='black'/>
+                        <Typography sx={{ color: 'black' }}>
+                            Edit experiment
+                        </Typography>
+                    </Button>
                 <Box sx={{ ml: 1 }}>
                     <Button variant="contained" color="error" href="/experiment/delete">
                         {/* href/onclick should open a dialog box asking for conformation for deletion*/}
@@ -20,14 +28,6 @@ const ExperimentHeader = () => {
                         </Typography>
                     </Button>
                 </Box>
-                <Box sx={{ ml: 1 }}>
-                    <Button variant="contained" color="info" onClick={handleEditClick}>
-                        {/* href/onclick should open a dialog box asking for conformation for edit*/}
-                        <IconEdit color='black'/>
-                        <Typography sx={{ color: 'black' }}>
-                            Edit experiment
-                        </Typography>
-                    </Button>
                 </Box>
             </Box>
         </Box>
