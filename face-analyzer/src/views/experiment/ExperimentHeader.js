4e38ce8e442df0802d1690b8113bae1ff19099a1
Jo Falck-Ytter <jofa@stud.ntnu.no>
2023-11-03 15:29:30 +0100

    button-click now sends user to new page no working modal for edit

--- face-analyzer/src/views/experiment/ExperimentHeader.js
+++ face-analyzer/src/views/experiment/ExperimentHeader.js
@@ -1,36 +1,40 @@
 import React from 'react';
-import { Button, Typography, Box } from '@mui/material';
+import { Button, Typography, Box, Grid } from '@mui/material';
 import { IconEdit, IconTrashOff } from '@tabler/icons';
 
 const ExperimentHeader = () => {
 
     return (
-        <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%' }}>
-            <Typography variant="h4" sx={{ mb: 2 }}>
-                {/* Text below should be loaded from backend */}
-                Experiment Title 
-            </Typography>
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                <Box sx={{ ml: 1 }}>
-                    <Button variant="contained" color="info" >
-                        {/* href/onclick should open a dialog box asking for conformation for edit*/}
-                        <IconEdit color='black'/>
-                        <Typography sx={{ color: 'black' }}>
-                            Edit experiment
-                        </Typography>
-                    </Button>
-                <Box sx={{ ml: 1 }}>
-                    <Button variant="contained" color="error" href="/experiment/delete">
-                        {/* href/onclick should open a dialog box asking for conformation for deletion*/}
-                        <IconTrashOff color='black' />
-                        <Typography sx={{ color: 'black' }}>
-                            Delete experiment
-                        </Typography>
-                    </Button>
+        <Grid container spacing={2} sx={{ mb: 2 }}>
+            <Grid item xs={6}>
+                <Typography variant="h4">
+                    {/* Text below should be loaded from backend */}
+                    Experiment Title 
+                </Typography>
+            </Grid>
+            <Grid item xs={6} sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    <Box sx={{ ml: 1 }}>
+                        <Button variant="contained" color="info" href="experiment/edit">
+                            {/* href/onclick should open a dialog box asking for conformation for edit*/}
+                            <IconEdit color='black'/>
+                            <Typography sx={{ color: 'black' }}>
+                                Edit experiment
+                            </Typography>
+                        </Button>
+                    </Box>
+                    <Box sx={{ ml: 1 }}>
+                        <Button variant="contained" color="error" href="/experiment/delete">
+                            {/* href/onclick should open a dialog box asking for conformation for deletion*/}
+                            <IconTrashOff color='black' />
+                            <Typography sx={{ color: 'black' }}>
+                                Delete experiment
+                            </Typography>
+                        </Button>
+                    </Box>
                 </Box>
-                </Box>
-            </Box>
-        </Box>
+            </Grid>
+        </Grid>
     );
 };
 
