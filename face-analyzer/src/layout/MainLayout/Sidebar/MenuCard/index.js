30cd309755911b91593447ad265083d723c33975
Jo Falck-Ytter <jofa@stud.ntnu.no>
2023-11-11 18:12:24 +0100

    annoying box gone.
    there might be code parts used in it still remaining, cleanup?

--- face-analyzer/src/layout/MainLayout/Sidebar/MenuCard/index.js
+++ face-analyzer/src/layout/MainLayout/Sidebar/MenuCard/index.js
@@ -3,21 +3,13 @@ import PropTypes from 'prop-types';
 // material-ui
 import { styled, useTheme } from '@mui/material/styles';
 import {
-  Avatar,
-  Card,
-  CardContent,
   Grid,
   LinearProgress,
-  List,
-  ListItem,
-  ListItemAvatar,
-  ListItemText,
   Typography,
   linearProgressClasses
 } from '@mui/material';
 
 // assets
-import TableChartOutlinedIcon from '@mui/icons-material/TableChartOutlined';
 
 // styles
 const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
@@ -32,22 +24,6 @@ const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
   }
 }));
 
-const CardStyle = styled(Card)(({ theme }) => ({
-  background: theme.palette.primary.light,
-  marginBottom: '22px',
-  overflow: 'hidden',
-  position: 'relative',
-  '&:after': {
-    content: '""',
-    position: 'absolute',
-    width: '157px',
-    height: '157px',
-    background: theme.palette.primary[200],
-    borderRadius: '50%',
-    top: '-105px',
-    right: '-96px'
-  }
-}));
 
 // ==============================|| PROGRESS BAR WITH LABEL ||============================== //
 
@@ -79,47 +55,3 @@ LinearProgressWithLabel.propTypes = {
   value: PropTypes.number
 };
 
-// ==============================|| SIDEBAR MENU Card ||============================== //
-
-const MenuCard = () => {
-  const theme = useTheme();
-
-  return (
-    <CardStyle>
-      <CardContent sx={{ p: 2 }}>
-        <List sx={{ p: 0, m: 0 }}>
-          <ListItem alignItems="flex-start" disableGutters sx={{ p: 0 }}>
-            <ListItemAvatar sx={{ mt: 0 }}>
-              <Avatar
-                variant="rounded"
-                sx={{
-                  ...theme.typography.commonAvatar,
-                  ...theme.typography.largeAvatar,
-                  color: theme.palette.primary.main,
-                  border: 'none',
-                  borderColor: theme.palette.primary.main,
-                  background: '#fff',
-                  marginRight: '12px'
-                }}
-              >
-                <TableChartOutlinedIcon fontSize="inherit" />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText
-              sx={{ mt: 0 }}
-              primary={
-                <Typography variant="subtitle1" sx={{ color: theme.palette.primary[800] }}>
-                  Get Extra Space
-                </Typography>
-              }
-              secondary={<Typography variant="caption"> 28/23 GB</Typography>}
-            />
-          </ListItem>
-        </List>
-        <LinearProgressWithLabel value={80} />
-      </CardContent>
-    </CardStyle>
-  );
-};
-
-export default MenuCard;
