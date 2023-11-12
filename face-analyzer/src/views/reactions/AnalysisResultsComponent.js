6e531315b3b18846d1638bb7d31792ccacc49866
jelence1 <jelena.gavran@fer.hr>
2023-11-12 20:38:57 +0100

    recording stimuli ux pretty

--- face-analyzer/src/views/reactions/AnalysisResultsComponent.js
+++ face-analyzer/src/views/reactions/AnalysisResultsComponent.js
@@ -2,8 +2,8 @@ import {useContext} from 'react';
 import {AnalysisDataContext} from "./AnalysisDataContext";
 
 // material-ui
-import { useTheme, styled } from '@mui/material/styles';
-import { Box, List, ListItem, ListItemText, Typography } from '@mui/material';
+import {styled, useTheme} from '@mui/material/styles';
+import {Box, List, ListItem, ListItemText, Typography} from '@mui/material';
 
 // project imports
 import MainCard from 'ui-component/cards/MainCard';
@@ -24,6 +24,7 @@ const AnalysisResultsComponent = ({ isLoading }) => {
     }
 
     //emotions = analysisData.getEmotionProbabilities();
+    const ROUND_TO_DIGITS = 5;
 
     return (
         <>
@@ -33,154 +34,57 @@ const AnalysisResultsComponent = ({ isLoading }) => {
                 <>
                     <CardWrapper border={false} content={false}>
                         <Box sx={{ p: 2 }}>
-                            <List sx={{ py: 0 }}>
-                                <ListItem alignItems="center" disableGutters sx={{ py: 0 }}>
-                                    <ListItemText
-                                        sx={{
-                                            py: 0,
-                                            mt: 0.45,
-                                            mb: 0.45
-                                        }}
-                                        primary={
-                                            <Typography
-                                                variant="subtitle2"
-                                                sx={{
-                                                    color: theme.palette.grey[500],
-                                                    mt: 0.5
-                                                }}
-                                            >
-                                                Anger
-                                            </Typography>
-                                        }
-                                        secondary={<Typography variant="h4"> {getLatestData(0)} </Typography>}
-                                    />
-                                </ListItem>
-                                <ListItem alignItems="center" disableGutters sx={{ py: 0 }}>
-                                    <ListItemText
-                                        sx={{
-                                            py: 0,
-                                            mt: 0.45,
-                                            mb: 0.45
-                                        }}
-                                        primary={
-                                            <Typography
-                                                variant="subtitle2"
-                                                sx={{
-                                                    color: theme.palette.grey[500],
-                                                    mt: 0.5
-                                                }}
-                                            >
-                                                Disgust
-                                            </Typography>
-                                        }
-                                        secondary={<Typography variant="h4"> {getLatestData(1)} </Typography>}
-                                    />
-                                </ListItem>
-                                <ListItem alignItems="center" disableGutters sx={{ py: 0 }}>
-                                    <ListItemText
-                                        sx={{
-                                            py: 0,
-                                            mt: 0.45,
-                                            mb: 0.45
-                                        }}
-                                        primary={
-                                            <Typography
-                                                variant="subtitle2"
-                                                sx={{
-                                                    color: theme.palette.grey[500],
-                                                    mt: 0.5
-                                                }}
-                                            >
-                                                Fear
-                                            </Typography>
-                                        }
-                                        secondary={<Typography variant="h4"> {getLatestData(2)} </Typography>}
-                                    />
-                                </ListItem>
-                                <ListItem alignItems="center" disableGutters sx={{ py: 0 }}>
-                                    <ListItemText
-                                        sx={{
-                                            py: 0,
-                                            mt: 0.45,
-                                            mb: 0.45
-                                        }}
-                                        primary={
-                                            <Typography
-                                                variant="subtitle2"
-                                                sx={{
-                                                    color: theme.palette.grey[500],
-                                                    mt: 0.5
-                                                }}
-                                            >
-                                                Happiness
-                                            </Typography>
-                                        }
-                                        secondary={<Typography variant="h4"> {getLatestData(3)} </Typography>}
-                                    />
-                                </ListItem>
-                                <ListItem alignItems="center" disableGutters sx={{ py: 0 }}>
-                                    <ListItemText
-                                        sx={{
-                                            py: 0,
-                                            mt: 0.45,
-                                            mb: 0.45
-                                        }}
-                                        primary={
-                                            <Typography
-                                                variant="subtitle2"
-                                                sx={{
-                                                    color: theme.palette.grey[500],
-                                                    mt: 0.5
-                                                }}
-                                            >
-                                                Sadness
-                                            </Typography>
-                                        }
-                                        secondary={<Typography variant="h4"> {getLatestData(4)} </Typography>}
-                                    />
-                                </ListItem>
-                                <ListItem alignItems="center" disableGutters sx={{ py: 0 }}>
-                                    <ListItemText
-                                        sx={{
-                                            py: 0,
-                                            mt: 0.45,
-                                            mb: 0.45
-                                        }}
-                                        primary={
-                                            <Typography
-                                                variant="subtitle2"
-                                                sx={{
-                                                    color: theme.palette.grey[500],
-                                                    mt: 0.5
-                                                }}
-                                            >
-                                                Surprise
-                                            </Typography>
-                                        }
-                                        secondary={<Typography variant="h4"> {getLatestData(5)} </Typography>}
-                                    />
-                                </ListItem>
-                                <ListItem alignItems="center" disableGutters sx={{ py: 0 }}>
-                                    <ListItemText
-                                        sx={{
-                                            py: 0,
-                                            mt: 0.45,
-                                            mb: 0.45
-                                        }}
-                                        primary={
-                                            <Typography
-                                                variant="subtitle2"
-                                                sx={{
-                                                    color: theme.palette.grey[500],
-                                                    mt: 0.5
-                                                }}
-                                            >
-                                                Neutral
-                                            </Typography>
-                                        }
-                                        secondary={<Typography variant="h4"> {getLatestData(6)} </Typography>}
-                                    />
-                                </ListItem>
+                            <List sx={{py: 0, display: 'flex', flexDirection: 'row'}}>
+                                {['Anger', 'Disgust', 'Fear', 'Happiness'].map((emotion, index) => (
+                                    <ListItem key={index} alignItems="center" disableGutters sx={{py: 0}}>
+                                        <ListItemText
+                                            sx={{
+                                                py: 0,
+                                                mt: 0.45,
+                                                mb: 0.45
+                                            }}
+                                            primary={
+                                                <Typography
+                                                    variant="subtitle2"
+                                                    sx={{
+                                                        color: theme.palette.grey[500],
+                                                        mt: 0.5
+                                                    }}
+                                                >
+                                                    {emotion}
+                                                </Typography>
+                                            }
+                                            secondary={<Typography
+                                                variant="h4"> {getLatestData(index).toFixed(ROUND_TO_DIGITS)} </Typography>}
+                                        />
+                                    </ListItem>
+                                ))}
+                            </List>
+                            <List sx={{py: 0, display: 'flex', flexDirection: 'row'}}>
+                                {['Sadness', 'Surprise', 'Neutral'].map((emotion, index) => (
+                                    <ListItem key={index + 4} alignItems="center" disableGutters sx={{py: 0}}>
+                                        <ListItemText
+                                            sx={{
+                                                py: 0,
+                                                mt: 0.45,
+                                                mb: 0.45
+                                            }}
+                                            primary={
+                                                <Typography
+                                                    variant="subtitle2"
+                                                    sx={{
+                                                        color: theme.palette.grey[500],
+                                                        mt: 0.5
+                                                    }}
+                                                >
+                                                    {emotion}
+                                                </Typography>
+                                            }
+                                            secondary={<Typography
+                                                variant="h4"> {getLatestData(index + 4).toFixed(ROUND_TO_DIGITS)} </Typography>}
+                                        />
+                                    </ListItem>
+                                ))}
                                 <ListItem alignItems="center" disableGutters sx={{ py: 0 }}>
                                     <ListItemText
                                         sx={{
@@ -199,7 +103,8 @@ const AnalysisResultsComponent = ({ isLoading }) => {
                                                 Time
                                             </Typography>
                                         }
-                                        secondary={<Typography variant="h4"> {getLatestData("time")} </Typography>}
+                                        secondary={<Typography
+                                            variant="h4"> {(getLatestData("time") / 1000).toFixed(ROUND_TO_DIGITS)} s </Typography>}
                                     />
                                 </ListItem>
                             </List>
