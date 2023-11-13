12dec0e47d8cc23e769c0fd43a6c3ba9a7245de5
lukamujagic <luka.mujagic@fer.hr>
2023-11-13 10:59:10 +0100

    Optimized put reactions
    Sends one API call to both send emotion data in an array and create a new reaction, instead of first creating the reaction and then sending the emotions one by one, thus saving network resources.

--- face-analyzer/src/views/reactions/AnalysisDataFunctions.js
+++ face-analyzer/src/views/reactions/AnalysisDataFunctions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import {ADD_EMOTION_API, ADD_REACTION_API} from "./BackendEndpoints";
+import {ADD_REACTION_API} from "./BackendEndpoints";
 import {DEFAULT_API_CONFIG} from "../projects/BackendEndpoints";
 
 let firstRun = true;
@@ -7,24 +7,9 @@ const dataKey = "analysisData"
 
 const emotion_types = ["Anger", "Disgust", "Fear", "Happiness", "Sadness", "Surprise", "Neutral"];
 
-const handleSaveEmotion = (values, /*{setErrors, setStatus}*/) => {
+const handleSaveReaction = (values) => {
     try{
-        axios.post(ADD_EMOTION_API, JSON.stringify(values), DEFAULT_API_CONFIG)
-            .then(response => {
-                if (response.status === 201) {
-                    console.log("Success");
-                }
-                else{
-                    console.error(response.data.errors);
-                }
-            });
-    } catch (e) {
-        console.error(e);
-    }
-};
-
-const handleSaveReaction = async (values) => {
-        return await axios.post(ADD_REACTION_API, JSON.stringify(values), DEFAULT_API_CONFIG)
+        axios.post(ADD_REACTION_API, JSON.stringify(values), DEFAULT_API_CONFIG)
             .then(response => {
                 if (response.status === 201 || response.status === 200) {
                     return response.data.id;
@@ -32,10 +17,12 @@ const handleSaveReaction = async (values) => {
                 else{
                     console.error(response.data.errors);
                 }
-            }).catch((e) => {
+            });
+    }
+    catch(e){
         console.error(e);
-    });
-}
+    }
+};
 
 export const saveDataToLocalStorage = (data) => {
     let retrieved = localStorage.getItem(dataKey);
@@ -57,27 +44,33 @@ export const saveNewReaction = async (stimuliId) => {
     let retrieved = localStorage.getItem(dataKey);
     let stored = JSON.parse(retrieved);
 
-    //Create new reaction
-    const tempReactionData = {
-        "stimuliId": stimuliId,
-        "participantName": "Stephan Horvat III"
-    };
-    await handleSaveReaction(tempReactionData).then((reactionId) => {
-        if (reactionId !== undefined) {
-            stored.forEach((emotionsData) => {
-                const time = emotionsData["time"];
-                for (let i = 0; i < 7; i++) {
-                    const tempEmotionData = {
-                        "value": emotionsData[i],
-                        "timeOffset": time,
-                        "emotionType": emotion_types[i],
-                        "reactionId": reactionId
-                    };
-                    handleSaveEmotion(tempEmotionData);
-                }
-            });
-        } else {
-            console.error("Error creating reaction.");
+    const tempReadings = [];
+
+    stored.forEach((emotionsData) => {
+        const time = emotionsData["time"];
+        if(!time)
+            return;
+
+        const values = {}
+        for (let key in emotion_types) {
+            values[emotion_types[key]] = emotionsData[key];
         }
+
+        const reading = {
+            time, values
+        };
+
+        tempReadings.push(reading);
     });
+
+    //TODO: add actual participant name
+    const forStoring = {
+        stimuliId: stimuliId,
+        participantName: "Participant Name",
+        emotionReadings: tempReadings
+    }
+
+    console.log("storing", forStoring);
+
+    handleSaveReaction(forStoring);
 };
