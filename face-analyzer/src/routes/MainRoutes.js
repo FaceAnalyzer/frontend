b9c76d05e2cea839cce20451d1488eb4360e881b
lukamujagic <luka.mujagic@fer.hr>
2023-11-16 21:06:32 +0100

    Removed unused imports in MainRoutes.js

--- face-analyzer/src/routes/MainRoutes.js
+++ face-analyzer/src/routes/MainRoutes.js
@@ -4,19 +4,12 @@ import {lazy} from 'react';
 import MainLayout from 'layout/MainLayout';
 import Loadable from 'ui-component/Loadable';
 
-// dashboard routing
-const DashboardDefault = Loadable(lazy(() => import('views/projects/experiments')));
-
-// analyzer routing
-const Analyzer = Loadable(lazy(() => import('views/reactions')));
-
 // 13 - user creates experiments routing
 const Experiments = Loadable(lazy(() => import('views/projects/experiments')));
 
 const Experiment  = Loadable(lazy(() => import('views/experiments')));
 const Stimuli = Loadable(lazy(() => import('views/stimuli')));
 const Stats = Loadable(lazy(() => import('views/stimuli/charts')));
-const Edit = Loadable(lazy(() => import('views/experiments/edit')));
 
 // ==============================|| MAIN ROUTING ||============================== //
 
