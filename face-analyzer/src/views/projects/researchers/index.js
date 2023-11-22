91f0ba3514548a3faef7fde4d6512ccad457e6ee
jelence1 <jelena.gavran@fer.hr>
2023-11-22 17:53:37 +0100

    make api calls to get researchers

--- face-analyzer/src/views/projects/researchers/index.js
+++ face-analyzer/src/views/projects/researchers/index.js
@@ -2,7 +2,11 @@
 import {Button, Grid} from "@mui/material";
 import React, {useEffect, useState} from "react";
 import axios from "axios";
-import {GET_PROJECT_BY_ID_API, GET_USERS_BY_PROJECT_ID_API} from "../../../endpoints/BackendEndpoints";
+import {
+    GET_PROJECT_BY_ID_API,
+    GET_RESEARCHERS_API,
+    GET_USERS_BY_PROJECT_ID_API
+} from "../../../endpoints/BackendEndpoints";
 import ProjectResearcherHeader from "./ResearcherHeader";
 import {useParams} from "react-router";
 import ResearcherDataGrid from "./ResearcherDataGrid";
@@ -14,7 +18,8 @@ const ProjectResearchers = () => {
     const {projectId} = useParams();
 
     const [isLoading, setLoading] = useState(true);
-    const [userList, setUserList] = useState([]);
+    const [usersOnProjectList, setUsersOnProjectList] = useState([]);
+    const [usersNotOnProjectList, setUsersNotOnProjectList] = useState([]);
     const [projectData, setProjectData] = useState({});
 
     useEffect(() => {
@@ -23,9 +28,16 @@ const ProjectResearchers = () => {
                 const projectResponse = await axios.get(GET_PROJECT_BY_ID_API.replace('{id}', projectId));
                 setProjectData(projectResponse.data);
 
-                const usersResponse = await axios.get(GET_USERS_BY_PROJECT_ID_API.replace('{id}', projectId));
-                const {items} = usersResponse.data;
-                setUserList(items);
+                const usersOnProjectResponse = await axios.get(GET_USERS_BY_PROJECT_ID_API.replace('{id}', projectId));
+                const usersOnProjectItems = usersOnProjectResponse.data.items;
+                setUsersOnProjectList(usersOnProjectItems);
+
+                const allUsersResponse = await axios.get(GET_RESEARCHERS_API);
+                const allUsers = allUsersResponse.data.items;
+
+                const usersNotOnProject = allUsers.filter(user => !usersOnProjectItems.some(projectUser => projectUser.id === user.id));
+
+                setUsersNotOnProjectList(usersNotOnProject);
 
                 setLoading(false);
             } catch (error) {
@@ -35,6 +47,8 @@ const ProjectResearchers = () => {
 
         fetchProjectAndUsers().then();
     }, [projectId]);
+
+    console.log(usersNotOnProjectList);
 
     return (
         <>
@@ -50,7 +64,7 @@ const ProjectResearchers = () => {
                     </Button>
                 </Grid>
             </Grid>
-            <ResearcherDataGrid isLoading={isLoading} userList={userList}/>
+            <ResearcherDataGrid isLoading={isLoading} userList={usersOnProjectList}/>
         </>
     );
 };
