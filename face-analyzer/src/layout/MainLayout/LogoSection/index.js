5675756bf0ffdbf487b04dd90cffaddafad03753
lukamujagic <luka.mujagic@fer.hr>
2023-12-15 12:53:25 +0100

    Experiment card and logo

--- face-analyzer/src/layout/MainLayout/LogoSection/index.js
+++ face-analyzer/src/layout/MainLayout/LogoSection/index.js
@@ -15,7 +15,7 @@ const LogoSection = () => {
   const defaultId = useSelector((state) => state.customization.defaultId);
   const dispatch = useDispatch();
   return (
-    <ButtonBase disableRipple onClick={() => dispatch({ type: MENU_OPEN, id: defaultId })} component={Link} to={config.defaultPath}>
+    <ButtonBase id="button-logo" disableRipple onClick={() => dispatch({ type: MENU_OPEN, id: defaultId })} component={Link} to={config.defaultPath}>
       <Logo />
     </ButtonBase>
   );
