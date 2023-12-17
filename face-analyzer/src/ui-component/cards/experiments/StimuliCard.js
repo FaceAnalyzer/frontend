8fc84c9e6553a4805eea3382f38508c071ce0b35
lukamujagic <luka.mujagic@fer.hr>
2023-12-17 09:20:33 +0100

    Stimulus cards

--- face-analyzer/src/ui-component/cards/experiments/StimuliCard.js
+++ face-analyzer/src/ui-component/cards/experiments/StimuliCard.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 // material-ui
 import {styled, useTheme} from '@mui/material/styles';
-import {Avatar, Box, Grid, Menu, MenuItem, Typography} from '@mui/material';
+import {Avatar, Box, CardActionArea, Grid, Menu, MenuItem, Typography} from '@mui/material';
 
 // project imports
 import MainCard from 'ui-component/cards/MainCard';
@@ -43,11 +43,11 @@ const StimuliCard = ({isLoading, data}) => {
         setAnchorEl(null);
     };
 
-    const openStimuliLink = () => {
+    const openStimulusLink = () => {
         window.open(stimulus.link, '_blank');
     };
 
-    const openStimuli = () => {
+    const openStimulus = () => {
         window.location.href = '/stimuli/' + stimulus.id;
     }
 
@@ -68,112 +68,141 @@ const StimuliCard = ({isLoading, data}) => {
                 <CardWrapper border={false} content={false}>
                     <DeleteStimuliModal showModal={showModal} closeModal={closeModal}
                                         data={stimulus}></DeleteStimuliModal>
-                    <Box sx={{ p: 2.25 }}>
-                        <Grid container direction="column">
-                            <Grid item>
-                                <Grid container justifyContent="space-between">
-                                    <Grid item>
-                                        <Grid container justifyContent="center">
-                                            <Grid item sx={{paddingRight: '5px'}}>
-                                                <Avatar
-                                                    id={"button-open-stimuli-" + stimulus.id}
-                                                    variant="rounded"
-                                                    sx={{
-                                                        ...theme.typography.commonAvatar,
-                                                        ...theme.typography.largeAvatar,
-                                                        backgroundColor: theme.palette.secondary.light,
-                                                        color: theme.palette.secondary.dark,
-                                                    }}
-                                                    onClick={openStimuli}
-                                                >
-                                                    <IconVideo/>
-                                                </Avatar>
-                                            </Grid>
-                                            <Grid item sx={{paddingLeft: '5px'}}>
-                                                <Avatar
-                                                    id={"link-stimuli-" + stimulus.id}
-                                                    variant="rounded"
-                                                    sx={{
-                                                        ...theme.typography.commonAvatar,
-                                                        ...theme.typography.largeAvatar,
-                                                        backgroundColor: theme.palette.secondary.light,
-                                                        color: theme.palette.secondary.dark,
-                                                    }}
-                                                    onClick={openStimuliLink}
-                                                >
-                                                    <YouTube/>
-                                                </Avatar>
+                    <CardActionArea onClick={openStimulus} id={"card-open-stimulus-" + stimulus.id}>
+                        <Box sx={{ p: 2.25 }}>
+                            <Grid container direction="column">
+                                <Grid item>
+                                    <Grid container justifyContent="space-between">
+                                        <Grid item>
+                                            <Grid container justifyContent="center">
+                                                <Grid item sx={{paddingRight: '5px'}}>
+                                                    <Avatar
+                                                        id={"button-open-stimulus-" + stimulus.id}
+                                                        variant="rounded"
+                                                        sx={{
+                                                            ...theme.typography.commonAvatar,
+                                                            ...theme.typography.largeAvatar,
+                                                            backgroundColor: theme.palette.secondary.light,
+                                                            color: theme.palette.secondary.dark,
+                                                        }}
+                                                        onClick={openStimulus}
+                                                    >
+                                                        <IconVideo/>
+                                                    </Avatar>
+                                                </Grid>
+                                                <Grid item sx={{paddingLeft: '5px'}}>
+                                                    <Avatar
+                                                        id={"link-stimulus-" + stimulus.id}
+                                                        variant="rounded"
+                                                        sx={{
+                                                            ...theme.typography.commonAvatar,
+                                                            ...theme.typography.largeAvatar,
+                                                            backgroundColor: theme.palette.secondary.light,
+                                                            color: theme.palette.secondary.dark,
+                                                        }}
+                                                        onMouseDown={event => event.stopPropagation()}
+                                                        onClick={event => {
+                                                            event.stopPropagation();
+                                                            event.preventDefault();
+                                                            openStimulusLink()
+                                                            }
+                                                        }
+                                                    >
+                                                        <YouTube/>
+                                                    </Avatar>
+                                                </Grid>
                                             </Grid>
                                         </Grid>
-                                    </Grid>
-                                    <Grid item>
-                                        <Avatar
-                                            id={"button-delete-stimuli-" + stimulus.id}
-                                            variant="rounded"
-                                            sx={{
-                                                ...theme.typography.commonAvatar,
-                                                ...theme.typography.mediumAvatar,
-                                                backgroundColor: theme.palette.secondary[800],
-                                                color: theme.palette.secondary[200],
-                                                zIndex: 1
-                                            }}
-                                            aria-controls="menu-stimuli-card"
-                                            aria-haspopup="true"
-                                            onClick={handleClick}
-                                        >
-                                            <MoreHorizIcon fontSize="inherit"/>
-                                        </Avatar>
-                                        <Menu
-                                            id="menu-stimuli-card"
-                                            anchorEl={anchorEl}
-                                            keepMounted
-                                            open={Boolean(anchorEl)}
-                                            onClose={handleClose}
-                                            variant="selectedMenu"
-                                            anchorOrigin={{
-                                                vertical: 'bottom',
-                                                horizontal: 'right'
-                                            }}
-                                            transformOrigin={{
-                                                vertical: 'top',
-                                                horizontal: 'right'
-                                            }}
-                                        >
-                                            <MenuItem onClick={openModal}
-                                                      sx={{color: 'red'}}>
-                                                <DeleteForever sx={{mr: 1.75}}/> Delete
-                                            </MenuItem>
-                                        </Menu>
+                                        <Grid item>
+                                            <Avatar
+                                                id={"button-delete-stimulus-" + stimulus.id}
+                                                variant="rounded"
+                                                sx={{
+                                                    ...theme.typography.commonAvatar,
+                                                    ...theme.typography.mediumAvatar,
+                                                    backgroundColor: theme.palette.secondary[800],
+                                                    color: theme.palette.secondary[200],
+                                                    zIndex: 1
+                                                }}
+                                                aria-controls="menu-stimulus-card"
+                                                aria-haspopup="true"
+                                                onMouseDown={event => event.stopPropagation()}
+                                                onClick={event => {
+                                                    event.stopPropagation();
+                                                    event.preventDefault();
+                                                    handleClick(event)
+                                                    }
+                                                }
+                                            >
+                                                <MoreHorizIcon fontSize="inherit"/>
+                                            </Avatar>
+                                            <Menu
+                                                id="menu-stimulus-card"
+                                                anchorEl={anchorEl}
+                                                keepMounted
+                                                open={Boolean(anchorEl)}
+                                                onClose={handleClose}
+                                                variant="selectedMenu"
+                                                anchorOrigin={{
+                                                    vertical: 'bottom',
+                                                    horizontal: 'right'
+                                                }}
+                                                transformOrigin={{
+                                                    vertical: 'top',
+                                                    horizontal: 'right'
+                                                }}
+                                                onMouseDown={event => event.stopPropagation()}
+                                                onClick={event => {
+                                                    event.stopPropagation();
+                                                    event.preventDefault();
+                                                    }
+                                                }
+                                            >
+                                                <MenuItem
+                                                    id={"menu-stimulus-" + stimulus.id + "-delete"}
+                                                    onMouseDown={event => event.stopPropagation()}
+                                                    onClick={event => {
+                                                        event.stopPropagation();
+                                                        event.preventDefault();
+                                                        openModal()
+                                                        }
+                                                    }
+                                                    sx={{color: 'red'}}
+                                                >
+                                                    <DeleteForever sx={{mr: 1.75}}/> Delete
+                                                </MenuItem>
+                                            </Menu>
+                                        </Grid>
                                     </Grid>
                                 </Grid>
-                            </Grid>
-                            <Grid item>
-                                <Grid container alignItems="center">
-                                    <Grid item>
-                                        <Typography
-                                            sx={{
-                                                fontSize: '2.125rem',
-                                                fontWeight: 500,
-                                                mr: 1,
-                                                mt: 1.75,
-                                                mb: 0.75
-                                            }}>{stimulus.name}</Typography>
+                                <Grid item>
+                                    <Grid container alignItems="center">
+                                        <Grid item>
+                                            <Typography
+                                                sx={{
+                                                    fontSize: '2.125rem',
+                                                    fontWeight: 500,
+                                                    mr: 1,
+                                                    mt: 1.75,
+                                                    mb: 0.75
+                                                }}>{stimulus.name}</Typography>
+                                        </Grid>
                                     </Grid>
                                 </Grid>
+                                <Grid item sx={{ mb: 1.25 }}>
+                                    <Typography
+                                        sx={{
+                                            fontSize: '1rem',
+                                            fontWeight: 500,
+                                            color: theme.palette.secondary[200]
+                                        }}
+                                    >
+                                        {stimulus.description}
+                                    </Typography>
+                                </Grid>
                             </Grid>
-                            <Grid item sx={{ mb: 1.25 }}>
-                                <Typography
-                                    sx={{
-                                        fontSize: '1rem',
-                                        fontWeight: 500,
-                                        color: theme.palette.secondary[200]
-                                    }}
-                                >
-                                    {stimulus.description}
-                                </Typography>
-                            </Grid>
-                        </Grid>
-                    </Box>
+                        </Box>
+                    </CardActionArea>
                 </CardWrapper>
       )}
     </>
